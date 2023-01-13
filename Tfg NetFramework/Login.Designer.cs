eac13fdd23943dd316ff64687ce50a2fd6562917
JuanjoLQ <jjlopezquinonero@gmail.com>
2023-01-13 08:45:53 +0100

    Login migrado

--- Tfg NetFramework/Login.Designer.cs
+++ Tfg NetFramework/Login.Designer.cs
@@ -1,16 +1,18 @@
-﻿namespace Tfg_NetFramework
+﻿using System.Windows.Forms;
+
+namespace Tfg_NetFramework
 {
     partial class Login
     {
         /// <summary>
-        /// Variable del diseñador necesaria.
+        ///  Required designer variable.
         /// </summary>
         private System.ComponentModel.IContainer components = null;
 
         /// <summary>
-        /// Limpiar los recursos que se estén usando.
+        ///  Clean up any resources being used.
         /// </summary>
-        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
         protected override void Dispose(bool disposing)
         {
             if (disposing && (components != null))
@@ -20,21 +22,156 @@
             base.Dispose(disposing);
         }
 
-        #region Código generado por el Diseñador de Windows Forms
+        #region Windows Form Designer generated code
 
         /// <summary>
-        /// Método necesario para admitir el Diseñador. No se puede modificar
-        /// el contenido de este método con el editor de código.
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Login));
+            this.tbPassword = new System.Windows.Forms.TextBox();
+            this.btnLogin = new System.Windows.Forms.Button();
+            this.tbEmail = new System.Windows.Forms.TextBox();
+            this.btnSalir = new System.Windows.Forms.Button();
+            this.cbLanguage = new System.Windows.Forms.ComboBox();
+            this.gbLogin = new System.Windows.Forms.GroupBox();
+            this.lbPassword = new System.Windows.Forms.Label();
+            this.lbEmail = new System.Windows.Forms.Label();
+            this.gbLogin.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tbPassword
+            // 
+            this.tbPassword.ForeColor = System.Drawing.Color.DimGray;
+            this.tbPassword.Location = new System.Drawing.Point(15, 106);
+            this.tbPassword.Name = "tbPassword";
+            this.tbPassword.Size = new System.Drawing.Size(90, 20);
+            this.tbPassword.TabIndex = 2;
+            this.tbPassword.Text = "Password";
+            this.tbPassword.GotFocus += new System.EventHandler(this.tbPassword_Focus);
+            this.tbPassword.LostFocus += new System.EventHandler(this.tbPassword_LostFocus);
+            // 
+            // btnLogin
+            // 
+            this.btnLogin.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnLogin.Location = new System.Drawing.Point(15, 137);
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Size = new System.Drawing.Size(79, 23);
+            this.btnLogin.TabIndex = 4;
+            this.btnLogin.Text = "Login";
+            this.btnLogin.UseVisualStyleBackColor = true;
+            this.btnLogin.Click += new System.EventHandler(this.btnLogin_Click);
+            // 
+            // tbEmail
+            // 
+            this.tbEmail.ForeColor = System.Drawing.Color.DimGray;
+            this.tbEmail.Location = new System.Drawing.Point(15, 55);
+            this.tbEmail.Name = "tbEmail";
+            this.tbEmail.Size = new System.Drawing.Size(157, 20);
+            this.tbEmail.TabIndex = 1;
+            this.tbEmail.Text = "Email";
+            this.tbEmail.GotFocus += new System.EventHandler(this.tbEmail_Focus);
+            this.tbEmail.LostFocus += new System.EventHandler(this.tbEmail_LostFocus);
+            // 
+            // btnSalir
+            // 
+            this.btnSalir.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnSalir.FlatAppearance.BorderSize = 0;
+            this.btnSalir.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnSalir.Font = new System.Drawing.Font("Comic Sans MS", 16.2F, System.Drawing.FontStyle.Bold);
+            this.btnSalir.Location = new System.Drawing.Point(407, 61);
+            this.btnSalir.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnSalir.Name = "btnSalir";
+            this.btnSalir.Size = new System.Drawing.Size(81, 28);
+            this.btnSalir.TabIndex = 7;
+            this.btnSalir.Text = "X";
+            this.btnSalir.UseVisualStyleBackColor = true;
+            this.btnSalir.Click += new System.EventHandler(this.btnSalir_Click);
+            this.btnSalir.ControlAdded += new System.Windows.Forms.ControlEventHandler(this.btnSalir_ControlAdded);
+            // 
+            // cbLanguage
+            // 
+            this.cbLanguage.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.cbLanguage.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbLanguage.FormattingEnabled = true;
+            this.cbLanguage.Items.AddRange(new object[] {
+            "EN - English",
+            "ES - Spanish"});
+            this.cbLanguage.Location = new System.Drawing.Point(178, 105);
+            this.cbLanguage.Name = "cbLanguage";
+            this.cbLanguage.Size = new System.Drawing.Size(115, 21);
+            this.cbLanguage.TabIndex = 8;
+            this.cbLanguage.SelectedIndexChanged += new System.EventHandler(this.cbLanguage_SelectedIndexChanged);
+            this.cbLanguage.SelectedValueChanged += new System.EventHandler(this.cbLanguage_SelectedValueChanged);
+            // 
+            // gbLogin
+            // 
+            this.gbLogin.BackColor = System.Drawing.Color.Transparent;
+            this.gbLogin.Controls.Add(this.lbPassword);
+            this.gbLogin.Controls.Add(this.cbLanguage);
+            this.gbLogin.Controls.Add(this.lbEmail);
+            this.gbLogin.Controls.Add(this.tbEmail);
+            this.gbLogin.Controls.Add(this.tbPassword);
+            this.gbLogin.Controls.Add(this.btnLogin);
+            this.gbLogin.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.gbLogin.Location = new System.Drawing.Point(174, 83);
+            this.gbLogin.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.gbLogin.Name = "gbLogin";
+            this.gbLogin.Padding = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.gbLogin.Size = new System.Drawing.Size(314, 175);
+            this.gbLogin.TabIndex = 9;
+            this.gbLogin.TabStop = false;
+            this.gbLogin.Text = "Please Login";
+            // 
+            // lbPassword
+            // 
+            this.lbPassword.AutoSize = true;
+            this.lbPassword.Location = new System.Drawing.Point(15, 83);
+            this.lbPassword.Name = "lbPassword";
+            this.lbPassword.Size = new System.Drawing.Size(56, 13);
+            this.lbPassword.TabIndex = 6;
+            this.lbPassword.Text = "Password:";
+            // 
+            // lbEmail
+            // 
+            this.lbEmail.AutoSize = true;
+            this.lbEmail.Location = new System.Drawing.Point(15, 30);
+            this.lbEmail.Name = "lbEmail";
+            this.lbEmail.Size = new System.Drawing.Size(38, 13);
+            this.lbEmail.TabIndex = 5;
+            this.lbEmail.Text = "Email: ";
+            // 
+            // Login
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(607, 377);
+            this.Controls.Add(this.btnSalir);
+            this.Controls.Add(this.gbLogin);
+            this.DoubleBuffered = true;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Name = "Login";
+            this.Text = "Login";
+            this.Load += new System.EventHandler(this.Login_Load);
+            this.gbLogin.ResumeLayout(false);
+            this.gbLogin.PerformLayout();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+        private TextBox tbPassword;
+        private Button btnLogin;
+        private TextBox tbEmail;
+        private Button btnSalir;
+        private ComboBox cbLanguage;
+        private GroupBox gbLogin;
+        private Label lbPassword;
+        private Label lbEmail;
     }
 }
 
--- Tfg NetFramework/Login.cs
+++ Tfg NetFramework/Login.cs
@@ -2,22 +2,136 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using capaEntidad;
+using capaNegocio;
 
 namespace Tfg_NetFramework
 {
     public partial class Login : Form
     {
-        
+        cnUser cnUser = new cnUser();
+
         public Login()
         {
+            ceGlobals.initializeDataUser();
             InitializeComponent();
         }
-        
+
+        private void tbEmail_Focus(object sender, EventArgs e)
+        {
+            if (tbEmail.Text == Res.Email)
+            {
+                tbEmail.Text = string.Empty;
+                tbEmail.ForeColor = Color.Black;
+            }
+        }
+
+        private void tbEmail_LostFocus(object sender, EventArgs e)
+        {
+            if (tbEmail.Text == string.Empty)
+            {
+                tbEmail.Text = Res.Email;
+                tbEmail.ForeColor = Color.DimGray;
+            }
+        }
+
+        private void tbPassword_Focus(object sender, EventArgs e)
+        {
+            if (tbPassword.Text == Res.Pass)
+            {
+                tbPassword.Text = string.Empty;
+                tbPassword.ForeColor = Color.Black;
+            }
+        }
+
+        private void tbPassword_LostFocus(object sender, EventArgs e)
+        {
+            if (tbPassword.Text == string.Empty)
+            {
+                tbPassword.Text = Res.Pass;
+                tbPassword.ForeColor = Color.DimGray;
+            }
+        }
+
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            if (cnUser.CheckUser(new ceUser(0, tbEmail.Text, tbPassword.Text)))
+            {
+                ceGlobals.email = tbEmail.Text;
+                ceGlobals.password = tbPassword.Text;
+
+                this.Hide();
+
+                HomePage homePage = new HomePage();
+                homePage.Show();
+
+            }
+        }
+
+        private void Login_Load(object sender, EventArgs e)
+        {
+            if (ceGlobals.lang == null)
+            {
+                Debug.WriteLine("Form load Lang Default");
+                ceGlobals.lang = "en-US";
+            }
+
+            System.Threading.Thread.CurrentThread.CurrentUICulture = new System.Globalization.CultureInfo(ceGlobals.lang);
+            cbLanguage.SelectedIndex = 0;
+            GetLanguage();
+        }
+
+        private void GetLanguage()
+        {
+            gbLogin.Text = Res.gbLogin;
+            tbEmail.Text = Res.user;
+            tbPassword.Text = Res.passwd;
+            btnLogin.Text = Res.Login;
+
+            /*º
+            lbRegister.Text = Res.Register;
+            tbREmail.Text = Res.Email;
+            tbRPassword.Text= Res.Pass;
+            btnRRegistrar.Text = Res.Register;   
+            */
+
+            //btnSalir.Text = Res.Exit;
+
+        }
+
+        private void cbLanguage_SelectedValueChanged(object sender, EventArgs e)
+        {
+            if (cbLanguage.SelectedItem.Equals("ES - Spanish"))
+            {
+                ceGlobals.lang = "";
+            }
+            else
+            {
+                ceGlobals.lang = "en-US";
+            }
+            System.Threading.Thread.CurrentThread.CurrentUICulture = new System.Globalization.CultureInfo(ceGlobals.lang);
+            GetLanguage();
+        }
+
+        private void btnSalir_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void btnSalir_ControlAdded(object sender, ControlEventArgs e)
+        {
+            btnSalir.BackColor = Color.Red;
+        }
+
+        private void cbLanguage_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
     }
 }
